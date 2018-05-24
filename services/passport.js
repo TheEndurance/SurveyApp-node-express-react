a3d1d4c1dcda88afbece8c8a498fa45032e36969
TheEndurance <rawa.jalal@gmail.com>
2018-05-24 15:06:12 -0400

    Call done() when user signs in from google

--- services/passport.js
+++ services/passport.js
@@ -12,7 +12,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken,refreshToken,profile,done)=> {
-            new User({googleId: profile.id}).save();
+            User.findOne({googleId:profile.id}).then((existingUser)=>{
+                   if (existingUser){
+                        done(null,user);
+                   } else {
+                       new User({googleId: profile.id}).save().then((user)=>{
+                           done(null,user);
+                       });
+                   }
+                });
+
         }
     )
 );
